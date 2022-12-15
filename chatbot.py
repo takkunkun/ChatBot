8500083dba774ed96d0b182d5a5996cc738ceadf
TakutoSato <oom2125064@stu.o-hara.ac.jp>
2022-12-15 20:03:45 +0900

    チャットボット初期コミット これから作るぞ！

---
+++ chatbot.py
@@ -0,0 +1,14 @@
+"""
+チャットボット
+第3回効果測定のやつを作り直す
+"""
+# 標準ライブラリ
+import datetime
+import random
+
+# 外部ライブラリ
+# https://qiita.com/skokado/items/50861b95b236068fd7b9
+# https://questionary.readthedocs.io/en/stable/index.html
+import questionary
+
+command_list=[]
