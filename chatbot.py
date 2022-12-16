1d52a8244919455e9c94e5c8a10b6af3ff2c07a4
TakutoSato <oom2125064@stu.o-hara.ac.jp>
2022-12-16 20:59:16 +0900

    「曜日当てゲーム」ができるようになった。

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,10 @@
 """
 チャットボット
 第3回効果測定のやつを作り直す
+python3.10で作成
 """
 # 標準ライブラリ
-import datetime
+from datetime import datetime
 import random
 
 # 外部ライブラリ
@@ -11,4 +12,59 @@ import random
 # https://questionary.readthedocs.io/en/stable/index.html
 import questionary
 
-command_list=[]
+
+class command:
+    def select() -> int:
+        """コマンド(したいこと)を選択。"""
+        ans = questionary.select(
+            '何をする？',
+            # choices=command_list,
+            choices=[
+                questionary.Choice(title="曜日当てゲーム", value=1),
+            ],
+            use_shortcuts=True,
+            use_jk_keys=False,
+            instruction="（数字キーまたは矢印キーを使用）"
+        ).ask(kbi_msg="キャンセルされました。")
+        command.do(ans)
+
+    def do(num: int) -> None:
+        match num:
+            case 1:
+                obj: object = today()
+                obj.do()
+            case _:
+                print("この番号は存在しません。")
+
+
+class today():
+    def __init__(self) -> None:
+        print("曜日当てゲームをします。")
+
+    days = ['月', '火', '水', '木', '金', '土', '日']
+    today = datetime.now().weekday()
+    day = days[today]
+
+    def do(self):
+        ans_day = questionary.select(
+            '今日は何曜日？',
+            choices=self.days,
+            use_shortcuts=True,
+            use_jk_keys=False,
+            instruction="（数字キーまたは矢印キーを使用）"
+        ).ask(kbi_msg="キャンセルされました。")
+
+        # キャンセルの場合、「None」が返ってくる
+        if not(ans_day == None):
+            if(ans_day == self.day):
+                print("正解")
+            else:
+                print("不正解")
+                print(f"正解は、「{self.day}」でした。")
+
+
+print('---チャットボットをスタートします---')
+print()
+command.select()
+print()
+print('---チャットボットが終了しました---')
