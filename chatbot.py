58196b302956ae7fc5f76a0c9adff87a03c27c46
TakutoSato <oom2125064@stu.o-hara.ac.jp>
2023-01-04 01:38:00 +0900

    自己紹介機能追加 「KeyboardInterrupt」追加

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-# 更新日：2022/12/29
+# 更新日：2022/01/04
 """
 チャットボット
 第3回効果測定のやつを作り直す
@@ -12,11 +12,18 @@ from time import sleep
 # https://qiita.com/skokado/items/50861b95b236068fd7b9
 # https://questionary.readthedocs.io/en/stable/index.html
 import questionary
+# 文字色を変えるライブラリ
+from colorama import Back, Fore, Style, init
 
 # 別ファイル
 from RockScissorsPaper import RockScissorsPaper
 from today import today
 from zipcode import zipcode
+from speak import speak
+from self_introduction import self_introduction
+
+# 文字色を自動で元に戻す
+init(autoreset=True)
 
 
 class command:
@@ -25,9 +32,10 @@ class command:
         ans = questionary.select(
             '何をする？',
             choices=[
-                questionary.Choice(title="曜日当てゲーム", value=1),
-                questionary.Choice(title="じゃんけんゲーム", value=2),
-                questionary.Choice(title="郵便番号から住所検索", value=3),
+                questionary.Choice(title="自己紹介", value=1),
+                questionary.Choice(title="曜日当てゲーム", value=2),
+                questionary.Choice(title="じゃんけんゲーム", value=3),
+                questionary.Choice(title="郵便番号から住所検索", value=4),
             ],
             # 数字キーまたは矢印キーで選択できるようにする。
             use_shortcuts=True,
@@ -39,17 +47,21 @@ class command:
             self.do(ans)
 
     def do(self, num: int) -> None:
+        """コマンド実行"""
         obj: Any
         match num:
             case 0:
                 pass
             case 1:
-                obj = today()
+                obj = self_introduction()
                 obj.do()
             case 2:
-                obj = RockScissorsPaper()
+                obj = today()
                 obj.do()
             case 3:
+                obj = RockScissorsPaper()
+                obj.do()
+            case 4:
                 obj = zipcode()
                 obj.do()
             case _:
@@ -69,12 +81,16 @@ def bool() -> Any:
 print('---チャットボットをスタートします---')
 
 while True:
-    print()
-    c = command()
-    c.select()
-    sleep(1)
-    is_bool = bool()
-    print()
-    if any([is_bool is False, is_bool is None]):
+    try:
+        print()
+        c = command()
+        c.select()
+        sleep(1)
+        is_bool = bool()
+        if any([is_bool is False, is_bool is None]):
+            break
+    except KeyboardInterrupt:
+        speak.user2(Back.LIGHTBLUE_EX + Fore.BLACK + "'Ctrl + C'で停止しました。")
         break
+print()
 print('---チャットボットが終了しました---')
