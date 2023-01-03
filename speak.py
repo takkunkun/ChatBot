a5d7c22a1acb25339d39371a84099b699615d4ab
TakutoSato <oom2125064@stu.o-hara.ac.jp>
2023-01-04 01:36:08 +0900

    ユーザーに入力を求める関数追加 ファイルに保存してあるユーザーネームを取得する。を関数化

--- speak.py
+++ speak.py
@@ -1,4 +1,6 @@
-# 更新日：2022/12/29
+# 更新日：2023/01/03
+# 作成日：2022/12/29
+
 # 標準ライブラリ
 import unicodedata
 
@@ -8,20 +10,23 @@ from colorama import Back, Fore, Style, init
 init(autoreset=True)
 
 # 初期値
-BotName = 'python'  # ボットの名前
-UserName = 'you'  # ユーザーの名前
+BotName = "python"  # ボットの名前
+UserName = "you"  # ユーザーの名前
 
-# ファイルに保存してあるユーザーネームを取得する。
-try:
-    f = open('username.txt', 'r', encoding='utf-8')
-except OSError:
-    pass
-else:
-    line = f.readline()
-    # 改行コードが在ったら空白にする。
-    line = line.replace('\n', '')
-    UserName = line
-    f.close()
+
+def get_username() -> (str):
+    """ファイルに保存してあるユーザーネームを取得する。"""
+    try:
+        f = open("username.txt", "r", encoding="utf-8")
+    except OSError:
+        UserName = ""
+    else:
+        line = f.readline()
+        # 改行コードが在ったら空白にする。
+        line = line.replace("\n", "")
+        UserName = line
+        f.close()
+    return UserName
 
 
 def len_str(str: str) -> int:
@@ -32,12 +37,18 @@ def len_str(str: str) -> int:
     """
     count = 0
     for c in str:
-        if unicodedata.east_asian_width(c) in ('F', 'W', 'A'):
+        if unicodedata.east_asian_width(c) in ("F", "W", "A"):
             count += 2
         else:
             count += 1
     return count
 
+
+UN = get_username()
+if UN != "":
+    UserName = UN
+
+del UN
 
 len_BotName = len_str(BotName)
 len_UserName = len_str(UserName)
@@ -52,7 +63,7 @@ class speak:
     @staticmethod
     def bot(speak: str) -> None:
         """ボットが話す1行目関数"""
-        print('{} > {}'.format(BotName, speak))
+        print("{} > {}".format(BotName, speak))
 
     @staticmethod
     def bot2(speak: str) -> None:
@@ -63,15 +74,15 @@ class speak:
     @staticmethod
     def bot_error(speak: str) -> None:
         """ボットが話す1行目関数(文字色赤)"""
-        print('{} > '.format(BotName) + Style.BRIGHT +
-              Fore.RED + '{}'.format(speak))
+        print("{} > ".format(BotName) + Style.BRIGHT +
+              Fore.RED + "{}".format(speak))
 
     @staticmethod
     def bot2_error(speak: str) -> None:
         """ボットが話す2行目以降関数(文字色赤)"""
         print(Style.BRIGHT +
-              Fore.RED + '%{}s  %s'.format(len_BotName + 1) %
-              ('', speak))
+              Fore.RED + "%{}s  %s".format(len_BotName + 1) %
+              ("", speak))
 
     @ staticmethod
     def user(speak: str) -> None:
@@ -83,6 +94,11 @@ class speak:
         """ユーザーが話す2行目以降関数"""
         print("%{}s  %s".format(len_UserName + 1) %
               ("", speak))
+
+    @ staticmethod
+    def user_input(speak: str) -> str:
+        """ユーザーに入力を求める関数"""
+        return input("{} > {}".format(UserName, speak))
 
 
 # メイン処理
